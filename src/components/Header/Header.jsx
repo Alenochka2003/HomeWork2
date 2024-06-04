3b3636ff0b578f12d4c0a1cd569f4db70886ce70
Alenochka2003 <alena.filina@icloud.com>
2024-06-04 16:47:50 +0200

    no coommit update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,3 @@
-// import styles from './Header.module.css';
-// import Logo from '../../assets/Logo.svg';
-// function Header() {
-//     return (
-//       <header className={styles.container}>
-//       <div className={styles.logo}>
-//         <img className={styles.logotip} src={Logo} />
-//           </div>
-//     </header> );
-// }
-
-// export default Header;
 
 import styles from './Header.module.css';
 import Logo from '../../assets/Logo.svg';
