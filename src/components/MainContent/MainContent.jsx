c9ddce926044087a7a732023169b95d4021bd6ad
Alenochka2003 <alena.filina@icloud.com>
2024-06-04 15:53:27 +0200

    add hrefs

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -14,15 +14,30 @@ function MainContent() {
             </a>
             <div className={styles.form_container}>
             <form className={styles.form_buttons}>
-            <button>
+            <a href="https://www.apple.com/de-edu/store?afid=p238%7Cs2ikzkU1J-dc_mtid_187079nc38483_pcrid_700307355074_pgrid_15885415519_pntwk_g_pchan__pexid__ptid_kwd-327275610644_&cid=aos-de-kwgo-edu--slid---product-" target="_blank" rel="noopener noreferrer">
+                        <button type="button">
+                            <img src={Apple} alt="Apple" />
+                        </button>
+                    </a>
+            {/* <button>
             <img src={Apple} alt="Apple" />
-        </button>
-        <button>
+        </button> */}
+        {/* <button>
             <img src={Google} alt="Google" />
-        </button>
-        <button>
+        </button> */}
+                 <a href="https://translate.google.com/?sl=en&tl=ru&op=translate">
+                        <button type="button">
+                            <img src={Google} alt="Google" />
+                        </button>
+                    </a>
+                    <a href="https://www.xbox.com/de-DE">
+                        <button type="button">
+                            <img src={Xbox} alt="Xbox" />
+                        </button>
+                    </a>
+        {/* <button>
             <img src={Xbox} alt="Xbox" />
-        </button>
+        </button> */}
                 </form>
               </div>
         </div>
